9f881ffead9fa08f110f8380486e1634386cef12
PG2697 <47768685+PG2697@users.noreply.github.com>
2019-02-22 10:54:29 +0530

    Update ARVer2.c

--- ARVer2.c
+++ ARVer2.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-	
 	//fixed array size and its elements
   int len=0,i,a[]={5,4,3,2,1};
-	
 	//storing the array in another array for further operations on the original array
 	for(i=0;i<len;i++)
 	{
 		b[i]=a[i];
 	}
-	
 	//reversing the array
 	for(i=0;i<len;i++)
 	{
 		a[i]=b[len-1-i];
 	}
-	
 	//displaying the original and reversed array
 	printf("\n The original array is:");
 	for(i=0;i<len;i++)
